e1de33998786d42301a780f6f47ac375c0ea5e84
박성규 <stargyu@yonsei.ac.kr>
2019-05-07 21:46:26 +0900

    init

---
+++ tags.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define FALL_THROUGH
+#define EMPTY_STATEMENT
+
+namespace def
+{
+	enum __tag
+	{
+		width = 800,
+		height = 600,
+
+		StrMaxLength = 1024,
+
+		InvalidValue = -12345,
+
+		Default_Order = -1000,
+	};
+}
